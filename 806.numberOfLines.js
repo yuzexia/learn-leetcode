cf1a81cd22c4ca8a3ba2074413d2d6261a462793
yuzexia <327932801@qq.com>
2022-04-13 21:00:49 +0800

    806.numberOfLines.js

--- 806.numberOfLines.js
+++ 806.numberOfLines.js
@@ -1,7 +1,9 @@
 /**
  * 806. 写字符串需要的行数
  * 
- * 我们要把给定的字符串 S从左到右写到每一行上，每一行的最大宽度为100个单位，如果我们在写某个字母的时候会使这行超过了100 个单位，那么我们应该把这个字母写到下一行。我们给定了一个数组widths，这个数组widths[0] 代表 'a' 需要的单位，widths[1] 代表 'b' 需要的单位，...，widths[25] 代表 'z' 需要的单位。
+ * 我们要把给定的字符串 S从左到右写到每一行上，每一行的最大宽度为100个单位，
+ * 如果我们在写某个字母的时候会使这行超过了100 个单位，那么我们应该把这个字母写到下一行。
+ * 我们给定了一个数组widths，这个数组widths[0] 代表 'a' 需要的单位，widths[1] 代表 'b' 需要的单位，...，widths[25] 代表 'z' 需要的单位。
 
 链接：https://leetcode-cn.com/problems/number-of-lines-to-write-string
 */
@@ -21,7 +23,7 @@ var numberOfLines = function(widths, s) {
 		width += len;
 		if(width > MAX_WIDTH) {
 			line++;
-			width = len;
+			width = len; // 如果我们在写某个字母的时候会使这行超过了100 个单位，那么我们应该把这个字母写到下一行。
 		}
 	}
 	console.log('result', [line, width]);
