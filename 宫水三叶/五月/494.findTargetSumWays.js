cd9eb9841043b224bd7c60eb907af26d0f15ef3e
yuzexia <327932801@qq.com>
2022-05-10 00:49:52 +0800

    494.findTargetSumWays.js

--- 宫水三叶/五月/494.findTargetSumWays.js
+++ 宫水三叶/五月/494.findTargetSumWays.js
@@ -15,8 +15,29 @@
  * @param {number} target
  * @return {number}
  */
-var findTargetSumWays = function(nums, target) {
+ /** 回溯方法
+  * 方法一：回溯
+	数组 nums 的每个元素都可以添加符号 + 或 -，因此每个元素有 2 种添加符号的方法，n 个数共有 2^n种添加符号的方法，对应 2^n种不同的表达式。
+	当 n 个元素都添加符号之后，即得到一种表达式，如果表达式的结果等于目标数 target，则该表达式即为符合要求的表达式。
 
+	可以使用回溯的方法遍历所有的表达式，回溯过程中维护一个计数器 count，当遇到一种表达式的结果等于目标数 target 时，将 count 的值加 1。
+	遍历完所有的表达式之后，即可得到结果等于目标数 target 的表达式的数目。
+  * 
+  */
+var findTargetSumWays = function(nums, target) {
+	let count = 0;
+	const backtrack = function(nums, target, index, sum) {
+		if(index === nums.length) {
+			if(sum === target) {
+				count++;
+			}
+		} else {
+			backtrack(nums, target, index + 1, sum + nums[index]);
+			backtrack(nums, target, index + 1, sum - nums[index]);
+		}
+	}
+	backtrack(nums, target, 0, 0);
+	return count;
 };
 
 var nums = [1,1,1,1,1];
